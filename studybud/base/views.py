e0c0f0ec55b98685f62107f79bd7cea74684d783
small__clock <257988550@qq.com>
2022-12-12 15:01:41 +0800

    Datebase & Admin Panel

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': "Let's learn python"},
-    {'id': 2, 'name': "Design with me"},
-    {'id': 3, 'name': "Backend Developers"}
-]
+# rooms = [
+#     {'id': 1, 'name': "Let's learn python"},
+#     {'id': 2, 'name': "Design with me"},
+#     {'id': 3, 'name': "Backend Developers"}
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    return render(request, 'base/room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
